53df11da818c6b6a46a024069acd59bfd4c83992
RuhamaMalik <malikruhama7@gmail.com>
2024-09-17 21:58:00 +0500

    welcome screen complete

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import WelcomeScreen from "./components/welcome-page/WelcomePage";
+function App() {
+  return (
+    <>
+      <WelcomeScreen>Hello World</WelcomeScreen>
+    </>
+  );
+}
+
+export default App;
